954cbaa684be36a849c64e78839f84f43d815d14
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2022-09-04 18:44:53 +0200

    [FEATURE] added login form to home page

--- frontend/src/components/Form.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-const StyledForm = styled.form``
-
-const Form = (props) => {
-  return <StyledForm>{props.children}</StyledForm>
-}
-
-export default Form
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,7 +1,35 @@
-import React from "react"
+import { useState } from "react"
+
+import Layout from "../components/styled/Layout"
+import Title from "../components/styled/Title"
+import Form from "../components/styled/Form"
 
 const Login = () => {
-  return <div>Login</div>
+  const [username, setUsername] = useState("")
+
+  function submitHandler(e) {
+    e.preventDefault()
+    console.log(username)
+  }
+
+  return (
+    <Layout>
+      <Title>Uni</Title>
+      <Form onSubmit={submitHandler}>
+        <label>
+          <input
+            type="text"
+            required
+            name={`Username`}
+            value={username}
+            placeholder="Nickname"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
+    </Layout>
+  )
 }
 
 export default Login
